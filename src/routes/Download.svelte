f4d5849c6131fb4f243f4bfcf6a80599946d50cc
DragonRatTiger <dragonrattiger@gmail.com>
2026-03-31 12:42:41 -0400

    Update Download.svelte

--- src/routes/Download.svelte
+++ src/routes/Download.svelte
@@ -162,7 +162,7 @@
                     text="Fetching..."
                     color1="rgb(80, 80, 80)"
                     color2="rgb(60, 60, 60)"
-                    href="https://drive.google.com/file/d/1D1ACI9J1hdA-b9cJtgN8f45EXO4jK2aw/view?usp=drive_link"
+                    href="https://drive.google.com/file/d/1IHn3NQHyVonqZtCFrmIf2BEWekIEA-Kv/view?usp=drive_link"
                 />
                 <ButtonLink
                     text="Fetching..."
@@ -179,7 +179,7 @@
             {:else}
                 <ButtonLink
                     text="Open-World Memories (v{skinver_owm})"
-                    href="https://drive.google.com/file/d/1XdTPEvn8wbFY0xt1Zv7shV1gT-LBfBDh/view?usp=drive_link"
+                    href="https://drive.google.com/file/d/1IHn3NQHyVonqZtCFrmIf2BEWekIEA-Kv/view?usp=drive_link"
                 />
                 <ButtonLink
                     text="SimpleStyle (1080p) (v{skinver_simple1080})"
